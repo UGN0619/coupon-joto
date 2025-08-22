a61b47be2bb034ddf95180b084f7477b76230b9d
Uugantogtokh <uugantogtokh.otgonbaatar@unimedia.mn>
2025-08-22 17:26:40 +0800

    fix auth logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,14 @@
 import { NextResponse } from "next/server";
 
-const USERNAME = "joto";
-const PASSWORD = "joto_2026";
+const USERNAME = process.env.AUTH_USERNAME!;
+const PASSWORD = process.env.AUTH_PASSWORD!;
+const isProd = process.env.NODE_ENV === "production";
 
 export function middleware(request: Request) {
+  if (!isProd) {
+    return NextResponse.next();
+  }
+
   const authHeader = request.headers.get("authorization");
   const expected =
     "Basic " + Buffer.from(`${USERNAME}:${PASSWORD}`).toString("base64");
